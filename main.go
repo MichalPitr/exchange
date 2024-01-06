26fa8882c3a4b3f1c7a3a875a283f1ff6f7129b8
michal pitr <michal.pitr.altis@gmail.com>
2024-01-06 16:40:37 +0100

    add order processing queue

--- main.go
+++ main.go
@@ -4,19 +4,72 @@ import (
 	"context"
 	"log"
 	"net"
+	"time"
 
 	pb "github.com/MichalPitr/exchange/protos"
 	"google.golang.org/grpc"
+
+	"github.com/MichalPitr/exchange/orderbook"
 )
 
 type server struct {
 	pb.UnimplementedOrderServiceServer
+	orderQueue chan orderbook.Order
 }
 
 func (s *server) SendOrder(ctx context.Context, in *pb.OrderRequest) (*pb.OrderResponse, error) {
 	log.Printf("Received: %v", in)
+	resultChan := make(chan orderbook.OrderResult, 1)
 	// Process the order here
-	return &pb.OrderResponse{Status: "Success", Details: "Order processed"}, nil
+	order := orderbook.Order{
+		UserID:     in.UserId,
+		Type:       in.Type,
+		OrderType:  in.OrderType,
+		Amount:     in.Amount,
+		Price:      in.Price,
+		Time:       time.Now().UnixNano(),
+		ResultChan: resultChan,
+	}
+
+	// Enqueue the order.
+	select {
+	case s.orderQueue <- order:
+		// Order enqueued successfully
+	case <-ctx.Done():
+		// Handle context cancellation
+		return nil, ctx.Err()
+	}
+
+	log.Printf("Enqueued order: %v\n", s.orderQueue)
+
+	var result orderbook.OrderResult
+	select {
+	case result = <-resultChan:
+		// Received the processing result
+	case <-ctx.Done():
+		// Handle context cancellation
+		return nil, ctx.Err()
+	}
+
+	if result.Success {
+		return &pb.OrderResponse{Status: "Success", Details: result.Message}, nil
+	} else {
+		return &pb.OrderResponse{Status: "Failed", Details: result.Message}, nil
+	}
+}
+
+func newServer(queueSize int) *server {
+	return &server{
+		orderQueue: make(chan orderbook.Order, queueSize),
+	}
+}
+
+func processOrders(queue <-chan orderbook.Order) {
+	for order := range queue {
+		// Process the order
+		log.Printf("Processing order: %v", order)
+		order.ResultChan <- orderbook.OrderResult{Message: "Processed", Success: true}
+	}
 }
 
 func main() {
@@ -25,7 +78,12 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	pb.RegisterOrderServiceServer(s, &server{})
+	srv := newServer(32)
+
+	go processOrders(srv.orderQueue)
+
+	pb.RegisterOrderServiceServer(s, srv)
+
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
--- orderbook/orderbook.go
+++ orderbook/orderbook.go
@@ -1,1 +1,16 @@
 package orderbook
+
+type Order struct {
+	UserID     int32
+	Type       string
+	OrderType  string
+	Amount     int32
+	Price      int64
+	Time       int64
+	ResultChan chan OrderResult
+}
+
+type OrderResult struct {
+	Success bool
+	Message string
+}
